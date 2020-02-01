4ad2cb4118c27c5e8c398536f064f63cddfa8733
Daniel Cregg <daniel.cregg@gmit.ie>
2020-02-01 22:40:12 +0000

    Adding css and JS

--- index.php
+++ index.php
@@ -2,13 +2,15 @@
 <html>
     <head>
         <link rel="stylesheet" href="styles.css">
+        
     </head>
     <body>
 
             <h1>My Website</h1>
-            <p>Hello World!</p>
-            <button>Hide the text above</button>
-            <script src="myscripts.js"></script>
+            <p id=demo>Hello World!</p>
+
+            <script type="text/javascript" src="myscripts.js"></script>
+            <button onclick="myFunction();">Click Me!</button>
             
     </body>
 </html>
