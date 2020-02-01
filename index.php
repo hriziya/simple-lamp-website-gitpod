30e9d18f1d1661fdc53d6f020f73d931905fe30d
Daniel Cregg <daniel.cregg@gmit.ie>
2020-02-01 22:18:25 +0000

    updates

--- index.php
+++ index.php
@@ -1,9 +1,14 @@
 <!DOCTYPE html>
 <html>
-        <body>
+    <head>
+        <link rel="stylesheet" href="styles.css">
+    </head>
+    <body>
 
-                <h1>My Website</h1>
-                <p>Hello World!</p>
-
-        </body>
+            <h1>My Website</h1>
+            <p>Hello World!</p>
+            <button>Hide the text above</button>
+            <script src="myscripts.js"></script>
+            
+    </body>
 </html>
