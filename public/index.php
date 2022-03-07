feda02d920865d5b5c1f2d8b3f71c1c4bb63e1b7
Daniel Cregg Gitpod <daniel.cregg@gmit.ie>
2022-03-07 11:58:09 +0000

    Make page simpler

--- public/index.php
+++ public/index.php
@@ -1,10 +1,7 @@
 <html>
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP - Hello, World!</title>
-</head>
-<body>
-        <h1><?php echo 'Hello, World!'; ?></h1>
-</body>
+    <head>
+    </head>
+    <body>
+            <h1><?php echo 'Hello, World!'; ?></h1>
+    </body>
 </html>
