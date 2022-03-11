71bdaee370c82edc1f27fb1a1eef8fb21c2e26b2
Hitesh Riziya <hriziya@gmail.com>
2022-03-11 19:59:53 +0000

    create pdf

--- public/index.php
+++ public/index.php
@@ -5,3 +5,9 @@
             <h1><?php echo 'Hello, World!'; ?></h1>
     </body>
 </html>
+<?php
+require_once __DIR__ . '/vendor/autoload.php';
+$mpdf = new \Mpdf\Mpdf();
+$mpdf->WriteHTML('<h1>Hello world!</h1>');
+$mpdf->Output();
+?>
